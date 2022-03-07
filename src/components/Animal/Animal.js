76f72d000b4e5884e872b7728ab358e34b150670
BrendenSeifried <brendenseifried@gmail.com>
2022-03-07 14:23:52 -0800

    forgot to commit before! tried one way until josh helped still need to resize animals

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,28 @@
 import React from 'react';
+//import animal from './animal.css';
 
-export default function Animal() {
-  return <></>;
+//import { animals } from '../data.js';
+
+
+
+export default function Animal({ name, says, top, left, type }) {
+  return (
+    <div className='animal' style={{ top, left }}>
+      <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`}/>
+      <span className="name">{name}</span>
+      <span>{says}</span>
+    </div>
+  );
 }
+
+// export default function Animal() {
+//   return (
+//     <>
+//       {animals.map((animal) => (
+//         <Animal key='' name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
+//       ))}
+//     </>
+//   );
+// }
+
+//---------Left the above code to show how I was originally doing it until Josh helped me out 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import './Main.css';
 
+import background from './background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
+
 export default function Main() {
-  return <main></main>;
+  return (
+    <div className="main" style={{ backgroundImage: `url(${background})` }}>
+      <main />
+      {animals.map((item) => (<Animal key={item.name} {...item} />))}
+    </div>
+  );
 }
+
+
+// export default function Main() {
+//   return (
+//     <div className="main" style={{ backgroundImage: `url(${background})` }}>
+//       <main />
+//     </div>
+//   );
+// }
+
+
+// import { animals } from '../data.js';
+
+// export default function Animal() {
+//   return (
+//     <>
+//       {animals.map((animal) => (
+//         <Animal key='' name={animal.name} type={animal.type} says={animal.says} top={animal.top} left={animal.left} />
+//       ))}
+//     </>
+//   );
+// }
+
